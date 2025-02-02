a4e70d9eb0f816fca1d9b40ee83121d2dd31acf6
mitgajere <gajeramit180@gmail.com>
2025-02-02 15:44:59 +0530

    Add Mongoose for MongoDB database integration

---
+++ lib/db.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL!;
+
+if(!MONGODB_URL){
+    throw new Error("MONGODB_URL is not defined");
+}
+
+let cached = global.mongoose;
+
+if(!cached){
+    cached = global.mongoose = {conn: null, promise: null}
+}
+
+export async function dbConnect() {
+
+    if(!cached.promise){
+        const opts = {
+            bufferCommands: true,
+            maxPoolSize: 10
+        }
+
+        cached.promise = mongoose
+        .connect(MONGODB_URL, opts)
+        .then(() => mongoose.connection);
+    } 
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (error) {
+        cached.promise = null;
+        throw error;
+    }
+
+    return cached.conn;
+}
